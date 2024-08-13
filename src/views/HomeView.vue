81e52cd4d5982ac5d0ade4e93a81a7c09943922c
mullerow <maximilian.kohler@posteo.de>
2024-08-13 12:09:13 +0200

    chore: now the drawn line is visible during the drawing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <div class="drawing-area" @mousedown="startDrawing" @mousemove="draw" @mouseup="stopDrawing">
+    <div class="drawing-area" @mousedown="startDrawing">
+      <svg v-if="points.length > 1" class="svg-drawing">
+        <polyline :points="pointsString" stroke="black" fill="none" />
+      </svg>
+
+      <svg class="svg-drawing">
+        <polyline
+          v-for="(shape, index) in this.svgpoints"
+          :key="index"
+          :points="shape"
+          stroke="black"
+          fill="none"
+        />
+      </svg>
       <div
         v-for="(shape, index) in shapes"
         :key="index"
@@ -13,51 +26,63 @@
         }"
       ></div>
     </div>
-    <div v-if="this.isDrawing">{{ this.points }}</div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
       isDrawing: false,
       points: [],
-      shapes: []
+      shapes: [],
+      svgPoints: [],
+      closedCercleAmount: 0
+    }
+  },
+  computed: {
+    pointsString() {
+      return this.points.map((point) => `${point.x},${point.y}`).join(' ')
     }
   },
   methods: {
     startDrawing(event) {
       this.isDrawing = true
       this.points = [{ x: event.offsetX, y: event.offsetY }]
-      console.log('Zeichnen gestartet', event.offsetX, event.offsetY)
-    },
 
+      window.addEventListener('mousemove', this.draw)
+      window.addEventListener('mouseup', this.stopDrawing)
+    },
     draw(event) {
       if (!this.isDrawing) return
 
       const newPoint = { x: event.offsetX, y: event.offsetY }
       this.points.push(newPoint)
-      console.log('Zeichnen läuft', newPoint)
+
       if (this.checkIfClosedShape(newPoint)) {
-        // this.stopDrawing()
+        this.stopDrawing()
         this.createShape()
       }
     },
     stopDrawing() {
       this.isDrawing = false
-      this.points = []
+      window.removeEventListener('mousemove', this.draw)
+      window.removeEventListener('mouseup', this.stopDrawing)
     },
     checkIfClosedShape(newPoint) {
-      if (this.points.length < 3) return false
+      if (this.points.length < 10) return false
 
       const firstPoint = this.points[0]
       const distance = Math.sqrt(
         Math.pow(newPoint.x - firstPoint.x, 2) + Math.pow(newPoint.y - firstPoint.y, 2)
       )
-      console.log('abstand bis geschlossen', distance)
-      return distance < 10 // Schwellenwert für die Erkennung eines geschlossenen Kreises
+      if (distance < 5) {
+        this.closedCercleAmount += 1
+      }
+      return distance < 5
     },
     createShape() {
+      /*
       const minX = Math.min(...this.points.map((point) => point.x))
       const minY = Math.min(...this.points.map((point) => point.y))
       const maxX = Math.max(...this.points.map((point) => point.x))
@@ -71,6 +96,10 @@ export default {
       }
 
       this.shapes.push(shape)
+      */
+      const svgShape = this.points.map((point) => `${point.x},${point.y}`).join(' ')
+      this.svgPoints.push(svgShape)
+      this.points = []
     }
   }
 }
@@ -82,6 +111,13 @@ export default {
   height: 500px;
   background-color: #f0f0f0;
   position: relative;
+}
+
+.svg-drawing {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  pointer-events: none; /* Verhindert, dass das SVG das Zeichnen blockiert */
 }
 
 .shape {
